77e5b6de84c747500b1348d80add05bdac84bf21
Simon Latusek <simon.latusek@gmail.com>
2023-07-20 00:28:54 +0200

    1.0 update I did ages ago apparently ?

--- Source/TextureReload/TextureReload/TextureReload.cs
+++ Source/TextureReload/TextureReload/TextureReload.cs
@@ -8,37 +8,50 @@ using Verse;
 
 namespace TextureReload
 {
+    using System.IO;
+
     [StaticConstructorOnStartup]
     public static class TextureReload
     {
         static TextureReload()
         {
-            HarmonyInstance.Create("rimworld.erdelf.TextureReload").Patch(AccessTools.Method(typeof(Dialog_DebugActionsMenu), "DoListingItems"), null, new HarmonyMethod(typeof(TextureReload), nameof(ReloadTextures)));
+            HarmonyInstance.Create("rimworld.erdelf.TextureReload").Patch(AccessTools.Method(typeof(Dialog_DebugActionsMenu), "DoListingItems"), null, new HarmonyMethod(typeof(TextureReload), nameof(ReloadTexturesButton)));
+
+            FileSystemWatcher watcher = new FileSystemWatcher(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.CreationTime | NotifyFilters.LastAccess | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Changed += (o, e) => ReloadTextures();
+            watcher.Created += (o, e) => ReloadTextures();
+            watcher.Deleted += (o, e) => ReloadTextures();
+            watcher.Renamed += (o, e) => ReloadTextures();
+
+            watcher.EnableRaisingEvents = true;
         }
 
-        static void ReloadTextures(Dialog_DebugActionsMenu __instance) => 
-            Traverse.Create(__instance as Dialog_DebugOptionLister).Method("DebugAction", "Reload Textures", new Action(() =>
-            {
-                GraphicDatabase.Clear();
-                LoadedModManager.RunningMods.Do(current =>
-                {
-                    Traverse textureInfo = Traverse.Create(current).Field("textures");
-                    ModContentHolder<Texture2D> textures = textureInfo.GetValue<ModContentHolder<Texture2D>>();
-                    textures.contentList.Clear();
-                    textures.ReloadAll();
-                    textureInfo.SetValue(textures);
-                    
-                    GenDefDatabase.DefsToGoInDatabase<ThingDef>(current).Do(def =>
-                    {
-                        if (def.graphicData != null)
-                        {
-                            Traverse.Create(def.graphicData).Method("Init").GetValue();
-                            def.graphic = def.graphicData.Graphic;
-                        }
-                    });
-                });
-                if (Current.ProgramState == ProgramState.Playing)
-                    Find.Maps.SelectMany(m => m.spawnedThings).Do(t => Traverse.Create(t).Field("graphicInt").SetValue(null));
-            })).GetValue();
+        static void ReloadTextures()
+        {
+            GraphicDatabase.Clear();
+            LoadedModManager.RunningMods.Do(current =>
+                                            {
+                                                Traverse                    textureInfo = Traverse.Create(current).Field("textures");
+                                                ModContentHolder<Texture2D> textures    = textureInfo.GetValue<ModContentHolder<Texture2D>>();
+                                                textures.contentList.Clear();
+                                                textures.ReloadAll();
+                                                textureInfo.SetValue(textures);
+
+                                                GenDefDatabase.DefsToGoInDatabase<ThingDef>(current).Do(def =>
+                                                                                                        {
+                                                                                                            if (def.graphicData != null)
+                                                                                                            {
+                                                                                                                Traverse.Create(def.graphicData).Method("Init").GetValue();
+                                                                                                                def.graphic = def.graphicData.Graphic;
+                                                                                                            }
+                                                                                                        });
+                                            });
+            if (Current.ProgramState == ProgramState.Playing)
+                Find.Maps.SelectMany(m => m.spawnedThings).Do(t => Traverse.Create(t).Field("graphicInt").SetValue(null));
+        }
+
+        static void ReloadTexturesButton(Dialog_DebugActionsMenu __instance) => 
+            Traverse.Create(__instance as Dialog_DebugOptionLister).Method("DebugAction", "Reload Textures", new Action(ReloadTextures)).GetValue();
     }
 }
--- Source/TextureReload/TextureReload/TextureReloadV2.cs
+++ Source/TextureReload/TextureReload/TextureReloadV2.cs
@@ -8,7 +8,8 @@ using Verse;
 
 namespace TextureReload
 {
-    //  [StaticConstructorOnStartup] This class does not work yet...
+    //  [StaticConstructorOnStartup]
+    // This class does not work yet...
     public static class TextureReloadV2
     {
         static List<WeakReference> graphics;
@@ -21,18 +22,18 @@ namespace TextureReload
 
             HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.TextureReload");
             harmony.Patch(AccessTools.Method(typeof(Dialog_DebugActionsMenu), "DoListingItems"), null, new HarmonyMethod(typeof(TextureReloadV2), nameof(ReloadTextures)));
-            typeof(Graphic).AllSubclassesNonAbstract().Do(t => 
-                    harmony.Patch(AccessTools.Method(t, "DrawWorker"), null, new HarmonyMethod(typeof(TextureReloadV2), nameof(AddGraphic))));
+            typeof(Graphic).AllSubclassesNonAbstract().Do(t => harmony.Patch(AccessTools.Method(t, "DrawWorker"), null, new HarmonyMethod(typeof(TextureReloadV2), nameof(AddGraphic))));
             harmony.Patch(AccessTools.Method(typeof(GraphicData), "Init"), null, new HarmonyMethod(typeof(TextureReloadV2), nameof(AddGraphic)));
         }
 
         static void AddGraphic(object __instance)
         {
-            //Log.Message(__instance.GetType().FullName);
-            if(__instance.GetType().IsSubclassOf(typeof(Graphic)))
-                if(!graphics.Any(wr => wr.Target == __instance))
+            Log.Message(__instance.GetType().FullName);
+            if (__instance.GetType().IsSubclassOf(typeof(Graphic)))
+            {
+                if (!graphics.Any(wr => wr.Target == __instance))
                     graphics.Add(new WeakReference(__instance));
-            else if(__instance.GetType() == typeof(GraphicData))
+            } else if(__instance.GetType() == typeof(GraphicData))
                     if (!graphicData.Any(wr => wr.Target == __instance))
                         graphicData.Add(new WeakReference(__instance));
         }
@@ -51,7 +52,7 @@ namespace TextureReload
                 });
                 //Find.Maps.SelectMany(m => m.spawnedThings).Do(t => Traverse.Create(t).Field("graphicInt").SetValue(null));
                 Log.Message(graphics.Count + " " + graphicData.Count);
-                graphicData.RemoveAll(wr => !wr.IsAlive);
+                Log.Message(graphicData.RemoveAll(wr => !wr.IsAlive).ToString());
                 graphicData.ForEach(wr => Traverse.Create(wr.Target).Method("Init").GetValue());
                 graphics.RemoveAll(wr => !wr.IsAlive);
                 graphics.ForEach(wr =>
@@ -62,8 +63,8 @@ namespace TextureReload
                     if (AccessTools.Method(graphic.GetType(), nameof(Graphic.Init)).DeclaringType != typeof(Graphic))
                     {
                         Log.Message("hey");
-                        graphic.Init(new GraphicRequest(graphic.GetType(), graphic.data?.texPath ?? graphic.path, graphic.data != null ? ShaderDatabase.ShaderFromType(graphic.data.shaderType) : graphic.Shader,
-                            graphic.data?.drawSize ?? graphic.drawSize, graphic.data?.color ?? graphic.color, graphic.data?.colorTwo ?? graphic.colorTwo, graphic.data, 0));
+                        graphic.Init(new GraphicRequest(graphic.GetType(), graphic.data?.texPath ?? graphic.path, graphic.data != null ? graphic.data.shaderType.Shader : graphic.Shader,
+                            graphic.data?.drawSize ?? graphic.drawSize, graphic.data?.color ?? graphic.color, graphic.data?.colorTwo ?? graphic.colorTwo, graphic.data, 0, graphic.data?.shaderParameters));
                     }
                 });
                 Log.Message(graphics.Count + " " + graphicData.Count);
